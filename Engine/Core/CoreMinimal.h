b7c3a84b686cd08ac8cbb5ad341125e4f26f9531
tglod <48178371+tglod@users.noreply.github.com>
2022-03-27 18:49:44 +0200

    Miejmy nadzieję, że teraz się zapisało

--- Engine/Core/CoreMinimal.h
+++ Engine/Core/CoreMinimal.h
@@ -6,8 +6,8 @@
 
 class ENGINE_API Graphics {
 	//Baza do tworzenia zasobów graficznych
-	std::vector<ID2D1Layer*> layers;
 	HWND* currentwindow;
+	std::vector<ID2D1Layer*> layers;
 	ID2D1Factory* factory;
 	ID2D1HwndRenderTarget* rendertarget;
 public:
@@ -19,8 +19,7 @@ public:
 	void EndDraw() { rendertarget->EndDraw(); }
 	void ClearScreen(float r, float g, float b);
 	void DrawCircle(float x, float y, float rad, float r, float g, float b, float a);
-	void CreateEllipseGeometry(D2D1_ELLIPSE &eli, ID2D1EllipseGeometry ** EllipseGeo);
+	void CreateEllipseGeometry(D2D1_ELLIPSE & eli, ID2D1EllipseGeometry ** ElipseGeo);
 	void DrawBG();
 	void DrawGeo(ID2D1EllipseGeometry* EllipseGeo);
-
 };
